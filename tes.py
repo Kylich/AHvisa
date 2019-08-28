675f044aaf43fcf2cb131455fd8d756f2d77c98e
Kylich <kylikov_nikita@mail.ru>
2019-08-28 21:21:34 +0300

    11

--- tes.py
+++ tes.py
@@ -4,8 +4,8 @@ from PIL import Image
 import pytesseract
 import sys
 
-sys.path.append('C:\\Program Files\\Tesseract-OCR\\')
-#pytesseract.pytesseract.tesseract_cmd = r'C:\\Program Files\\Tesseract-OCR\\'
+sys.path.append('C:\Program Files\Tesseract-OCR')
+pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
 
 image = Image.open('001.jpg')
 text = pytesseract.image_to_string(image, lang="rus")
