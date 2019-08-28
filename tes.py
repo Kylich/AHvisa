8f4f2964789d6d7e5ce3d5b0d6859209f4a406ef
Kylich <kylikov_nikita@mail.ru>
2019-08-28 18:54:00 +0300

    11

--- tes.py
+++ tes.py
@@ -1,10 +1,7 @@
-#!/bin/env python
 # -*- coding: utf-8 -*-.
  
-import os
 from PIL import Image
 import pytesseract
-import re
 
 image = Image.open('001.jpg')
 text = pytesseract.image_to_string(image, lang="rus")
