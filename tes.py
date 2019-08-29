d2ea8abf554497977ef4d29a61067e0764fbe0ae
Kylich <kylikov_nikita@mail.ru>
2019-08-29 14:56:55 +0300

    test folder

--- tes.py
+++ tes.py
@@ -8,11 +8,11 @@ sys.path.append('C:\Program Files\Tesseract-OCR')
 pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
 
 for photo in range(1,6):
-    #imageUP = Image.open('00%sup.jpg' % photo)
+    #imageUP = Image.open('scan test\\00%sup.jpg' % photo)
     #textUP = pytesseract.image_to_string(imageUP, lang="eng+rus")
     #print(textUP)
     
-    imageMCT = Image.open('00%smct.jpg' % photo)
+    imageMCT = Image.open('scan test\\00%smct.jpg' % photo)
     textMCT = pytesseract.image_to_string(imageMCT, lang="eng")
     textMCT = textMCT.replace(' ', '')
     
