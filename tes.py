0885ebb55a0a28c3832863e69e53d321abd25193
Kylich <kylikov_nikita@mail.ru>
2019-08-30 09:29:10 +0300

    dict tes

--- tes.py
+++ tes.py
@@ -1,15 +1,34 @@
 # -*- coding: utf-8 -*-.
-from PIL import Image
-import pytesseract
-pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+#from PIL import Image
+#import pytesseract
+#pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+#f = open('mct.txt', 'r', encoding='utf-8')
 
-for photo in range(1,6):
-    imageMCT = Image.open('scan test\\00%smct.jpg' % photo)
-    textMCT = pytesseract.image_to_string(imageMCT, lang="eng")
-    textMCT = textMCT.replace(' ', '')
+photo=[
+"""P<RUSCHUKINA<X<ELENA<<<<<<<<<<<<<<6£666KKKKKK
+7224947463RUS8407177F2212245<<<<<<<<<<<<<<06""",
+
+"""P<RUSURAEV<<ANDREY<<<<<<<<<<<<<esdsssssee<<<
+7234631288RUS6407276M2303093<<<<<<<<<<<<<<02""",
+
+"""P<RUSASTANIN<<SERGEY<<<<<<<<<<<<<<KKKKKKKKK
+7152939691RUS6405216M2107110<<<<<<<<<<<<<<08""",
+
+"""P<RUSDUBOVITSKAYA<<NATALIA<<<<<<<KKKKEKE
+5156003039RUS7312151F1710102<<<<cccceces<x<d""",
+
+"""P<RUSGRAN<<SERGEI<<<<<<<<<<<K<<Ke<<esss<<<<<<
+7179677969RUS8607151M2203023<<<<<<<<<<<<<<00"""]
+passAll = []
+
+
+for ph in photo:
+    #imageMCT = Image.open('scan test\\00%smct.jpg' % photo)
+    #textMCT = pytesseract.image_to_string(imageMCT, lang="eng")
+    #textMCT = textMCT.replace(' ', '')
+    passData = {}
+    MCTs = ph.split("\n")
     
-    MCTs = textMCT.split("\n")
-
     MCT1 = MCTs[0]
     MCT2 = MCTs[1]
 
@@ -20,26 +39,32 @@ for photo in range(1,6):
             KSmap.append(int(list(KS_)[i])*KSves[i])
         KSch = sum(KSmap) % 10
         if KSch != int(KS):
-            print(str(photo) + ' wrong KS ' + KS)
+            print('%s: wrong KS = %s' % ph, KS)
             input()
 
-    FNm = MCT1.find('<')
-    if MCT1[FNm+1] != '<':
-        FNm = MCT1.find('<', FNm+1)
+    FNm = MCT1.find('<', 2)
+    z=0
+    while True:
+
+        if MCT1[FNm+1] != '<':
+            MCT1 = MCT1[:FNm+1] + MCT1[FNm+2:]
+            if z>1:
+                print('%s: wrong FN' % ph)
+                input()
+            z+=1
+        else: break
+    
     FNe = MCT1.find('<', FNm+2)
 
     country = MCT1[2:5]
-    sername = MCT1[5:FNm]
+    surname = MCT1[5:FNm]
     name = MCT1[FNm+2:FNe]
-
     serial = MCT2[0:9]
+    
     KSch(MCT2[9], serial)
 
-    countryCh = MCT2[10:13]
-    if countryCh != country:
-        print(str(photo) + ' wrong country')
-        input()
-
+    Bcountry = MCT2[10:13]
+    
     KSch(MCT2[19], MCT2[13:19])
 
     Byear = MCT2[13:15]
@@ -50,3 +75,23 @@ for photo in range(1,6):
 
     passdate = MCT2[21:27]
     KSch(MCT2[27], passdate)
+
+    Pyear = MCT2[21:23]
+    Pmonth = MCT2[23:25]
+    Pday = MCT2[25:27]
+
+    passData ['country'] = country
+    passData ['surname'] = surname
+    passData ['name'] = name
+    passData ['serial'] = serial
+    passData ['Bcountry'] = Bcountry
+    passData ['Byear'] = Byear
+    passData ['Bmonth'] = Bmonth
+    passData ['Bday'] = Bday
+    passData ['Pyear'] = Pyear
+    passData ['Pmonth'] = Pmonth
+    passData ['Pday'] = Pday
+    
+    passAll.append(passData)
+
+#f.close
