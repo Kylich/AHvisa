2364004dacd96a87f0716c3b675c5cfa6286fa89
Kylich <kylikov_nikita@mail.ru>
2019-09-05 16:52:34 +0300

    111

--- mSQL.py
+++ mSQL.py
@@ -1,19 +1,8 @@
-# Импортируем библиотеку, соответствующую типу нашей базы данных 
 import sqlite3
 def newClient(client):
-    # Создаем соединение с нашей базой данных
-    # В нашем примере у нас это просто файл базы
     conn = sqlite3.connect('SQLvisa.db')
 
-    # Создаем курсор - это специальный объект который делает запросы и получает их результаты
     cursor = conn.cursor()
-
-    # cursor.execute("""CREATE TABLE clients
-    #                   (surname text, name text, sex text,
-    #                    country text, Bcountry text, Byear text,
-    #                    Bmonth text, Bday text, Pserial text, 
-    #                    Pyear text, Pmonth text, Pday text)""")
-
 
     try:
         cursor.execute("""INSERT INTO clients
